a23f1dac5b9b0ff13d7cd17b92c09c0685dba289
Marek Belski <marek.belski@gmail.com>
2026-08-05 11:09:18 -0700

    Remove deprecated update plugin methods in game template plugin script.

--- addons/maaacks_game_template/plugin.gd
+++ addons/maaacks_game_template/plugin.gd
@@ -245,39 +245,10 @@ func _open_continue_setup_dialog() -> void:
 	confirmation_instance.visibility_changed.connect(_on_visibility_changed_to_hidden.bind(confirmation_instance))
 	add_child(confirmation_instance)
 
-func _open_check_plugin_version() -> void:
-	if ProjectSettings.has_setting(PROJECT_SETTINGS_PATH + "disable_update_check"):
-		if ProjectSettings.get_setting(PROJECT_SETTINGS_PATH + "disable_update_check"):
-			return
-	else:
-		ProjectSettings.set_setting(PROJECT_SETTINGS_PATH + "disable_update_check", false)
-		ProjectSettings.save()
-	var check_version_scene : PackedScene = load(get_plugin_path() + "installer/check_plugin_version.tscn")
-	var check_version_instance : Node = check_version_scene.instantiate()
-	check_version_instance.auto_start = true
-	check_version_instance.new_version_detected.connect(_add_update_plugin_tool_option)
-	add_child(check_version_instance)
-
-func open_update_plugin() -> void:
-	var update_plugin_scene : PackedScene = load(get_plugin_path() + "installer/update_plugin.tscn")
-	var update_plugin_instance : Node = update_plugin_scene.instantiate()
-	update_plugin_instance.auto_start = true
-	update_plugin_instance.update_completed.connect(_remove_update_plugin_tool_option)
-	add_child(update_plugin_instance)
-
 func open_setup_wizard() -> void:
 	var setup_wizard_scene : PackedScene = load(get_plugin_path() + "installer/setup_wizard.tscn")
 	var setup_wizard_instance : Node = setup_wizard_scene.instantiate()
 	add_child(setup_wizard_instance)
-
-func _add_update_plugin_tool_option(new_version : String) -> void:
-	update_plugin_tool_string = "Update %s to v%s..." % [get_plugin_name(), new_version]
-	add_tool_menu_item(update_plugin_tool_string, open_update_plugin)
-
-func _remove_update_plugin_tool_option() -> void:
-	if update_plugin_tool_string.is_empty(): return
-	remove_tool_menu_item(update_plugin_tool_string)
-	update_plugin_tool_string = ""
 
 func _show_plugin_dialogues() -> void:
 	if not ProjectSettings.get_setting(PROJECT_SETTINGS_PATH + "disable_install_wizard", false):
@@ -324,11 +295,9 @@ func _install_audio_busses() -> void:
 
 func _add_tool_options() -> void:
 	add_tool_menu_item("Run " + get_plugin_name() + " Setup...", open_setup_wizard)
-	_open_check_plugin_version()
 
 func _remove_tool_options() -> void:
 	remove_tool_menu_item("Run " + get_plugin_name() + " Setup...")
-	_remove_update_plugin_tool_option()
 
 func _enter_tree() -> void:
 	_install_audio_busses()
