a09c1947225607f2051af009ba71bee4a1995dbd
Marek Belski <marek.belski@gmail.com>
2026-09-08 15:45:18 -0700

    Rsync with game template.

--- addons/maaacks_game_template/examples/scenes/game/levels/level.gd
+++ addons/maaacks_game_template/examples/scenes/game/levels/level.gd
@@ -1,6 +1,8 @@
 extends Node
 
+@warning_ignore("unused_signal")
 signal level_lost
+@warning_ignore("unused_signal")
 signal level_won(level_path : String)
 @warning_ignore("unused_signal")
 signal level_changed(level_path : String)
@@ -8,17 +10,17 @@ signal level_changed(level_path : String)
 ## Optional path to the next level if using an open world level system.
 @export_file("*.tscn") var next_level_path : String
 
-func _on_lose_button_pressed() -> void:
-	level_lost.emit()
-
-func _on_win_button_pressed() -> void:
-	level_won.emit(next_level_path)
+var level_state : LevelStateExample
 
 func open_tutorials() -> void:
 	%TutorialManager.open_tutorials()
+	level_state.tutorial_read = true
+	GlobalState.save()
 
 func _ready() -> void:
-	open_tutorials()
+	level_state = GameStateExample.get_level_state(scene_file_path)
+	if not level_state.tutorial_read:
+		open_tutorials()
 
 func _on_tutorial_button_pressed() -> void:
 	open_tutorials()
